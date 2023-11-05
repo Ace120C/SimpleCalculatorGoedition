1ef6d1b5484413ab4848293bf0d5b12c9e4acef2
Ace120C <145904614+Ace120C@users.noreply.github.com>
2023-11-05 22:28:13 +0100

    added a prompt that allows you to exit the app
    there was a bug where the app would crash after it shows the results, now it's fixed

--- calculator.go
+++ calculator.go
@@ -4,32 +4,42 @@ import "fmt"
 
 func main() {
 	var number1, number2, result float64
-	var operator string
 	fmt.Print("enter your number: ")
 	fmt.Scanln(&number1)
 	fmt.Print("enter another number: ")
 	fmt.Scanln(&number2)
+	calculation(number1, number2, &result)
+	fmt.Printf("Result: %f\n", result)
+	for {
+		var exit string = "exit"
+		fmt.Print("type exit to quit: ")
+		fmt.Scanln(&exit)
+		if exit == "exit" {
+			break
+		}
+	}
+}
+
+func calculation(number1, number2 float64, result *float64) {
+	var operator string
 	fmt.Print("choose an operator (+, -, x, /): ")
 	fmt.Scanln(&operator)
 	switch operator {
 	case "+":
-		result = number1 + number2
+		*result = number1 + number2
 	case "-":
-		result = number1 - number2
+		*result = number1 - number2
 	case "x":
-		result = number1 * number2
+		*result = number1 * number2
 	case "/":
 		if number2 == 0 {
 			println("Error: Division By Zero")
 			return
 		}
-		result = number1 / number2
+		*result = number1 / number2
 	default:
 		fmt.Println("Invalid Operator")
 		return
 	}
-	fmt.Printf("Result: %f\n", result)
-	var exit string = "exit"
-	fmt.Println("Press Any Button To Quit...")
-	fmt.Scanln(&exit)
+
 }
